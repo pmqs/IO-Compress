a89cb95284a3e4c59d95688bc37e10e7b431dcc2
pmqs <pmqs@cpan.org>
2012-04-29 12:42:10 +0100

    import IO-Compress 2.052
      2.052 29 April 2012
    
          * IO::Compress::Zip
            Force a ZIP64 archive when it contains >= 0xFFFF entries.
            
          * Typos in POD
            [RT# #76130: Gunzip Pod typo in OO section: $$output instead of $$input
    
    
    git-cpan-module:   IO-Compress
    git-cpan-version:  2.052
    git-cpan-authorid: pmqs

--- lib/IO/Compress/Zlib/Extra.pm
+++ lib/IO/Compress/Zlib/Extra.pm
@@ -8,9 +8,9 @@ use bytes;
 
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS);
 
-$VERSION = '2.049';
+$VERSION = '2.052';
 
-use IO::Compress::Gzip::Constants 2.049 ;
+use IO::Compress::Gzip::Constants 2.052 ;
 
 sub ExtraFieldError
 {
--- lib/IO/Uncompress/Adapter/Bunzip2.pm
+++ lib/IO/Uncompress/Adapter/Bunzip2.pm
@@ -4,12 +4,12 @@ use strict;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common 2.049 qw(:Status);
+use IO::Compress::Base::Common 2.052 qw(:Status);
 
-use Compress::Raw::Bzip2 2.049 ;
+use Compress::Raw::Bzip2 2.052 ;
 
 our ($VERSION, @ISA);
-$VERSION = '2.049';
+$VERSION = '2.052';
 
 sub mkUncompObject
 {
