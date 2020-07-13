d4daf10ca07ad0f91a31aa93de7948e977eedaa0
pmqs <pmqs@cpan.org>
2020-07-13 11:27:06 +0100

    version 2.094

--- lib/IO/Uncompress/Adapter/Identity.pm
+++ lib/IO/Uncompress/Adapter/Identity.pm
@@ -4,14 +4,14 @@ use warnings;
 use strict;
 use bytes;
 
-use IO::Compress::Base::Common  2.093 qw(:Status);
+use IO::Compress::Base::Common  2.094 qw(:Status);
 use IO::Compress::Zip::Constants ;
 
 our ($VERSION);
 
-$VERSION = '2.093';
+$VERSION = '2.094';
 
-use Compress::Raw::Zlib  2.093 ();
+use Compress::Raw::Zlib  2.094 ();
 
 sub mkUncompObject
 {
