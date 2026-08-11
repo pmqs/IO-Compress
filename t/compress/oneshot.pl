16006b48e11ecaf86482ab78895fe0a29be32b9b
Lukas Mai <lukasmai.403@gmail.com>
2026-08-12 00:38:46 +0200

    fix test skip message with threaded perl 5.6
    It makes more sense to print the perl version instead of a literal `$]`.

--- t/compress/oneshot.pl
+++ t/compress/oneshot.pl
@@ -115,7 +115,7 @@ sub run
             # Threaded 5.6.x seems to have a problem comparing filehandles.
             use Config;
 
-            skip 'Cannot compare filehandles with threaded $]', 2
+            skip "Cannot compare filehandles with threaded $]", 2
                 if $] >= 5.006  && $] < 5.007 && $Config{useithreads};
 
             my $lex = LexFile->new( my $out_file );
